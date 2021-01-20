2ab81d5056ddddd8c5278494163188e5eea9614b
es-develops <62126158+es-develops@users.noreply.github.com>
2021-01-19 19:14:49 -0500

    Update app.js
    added pad activated methods and functions

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
 class DrumKit {
   constructor() {
     this.pads = document.querySelectorAll(".pad");
+    this.playBtn = document.querySelector(".play");
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
+    this.bpm = 120;
+  }
+  // Actives pad color change
+  activePad() {
+    this.classList.toggle("active");
   }
   // Repeats over pads
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
+    console.log(step);
     this.index++;
   }
   start() {
+    const interval = (60 / this.bpm) * 1000;
     // Grabs repeat()
     setInterval(() => {
       this.repeat();
-    }, 1000); // 1000ms
+    }, interval); // in milliseconds
   }
 }
 
 const drumKit = new DrumKit();
 
-drumKit.start();
+// Pads become active when clicked
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+});
+
+// Starts start() when Play Button is Clicked
+drumKit.playBtn.addEventListener("click", () => {
+  drumKit.start();
+});
