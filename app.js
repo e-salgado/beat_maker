0970443df2dcc72a2ec952d7a713aa75aa345375
es-develops <62126158+es-develops@users.noreply.github.com>
2021-01-22 16:21:36 -0500

    Update
    Added tempo functionality

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ class DrumKit {
     this.isPlaying = null;
     this.selects = document.querySelectorAll("select");
     this.muteBtns = document.querySelectorAll(".mute");
+    this.tempoSlider = document.querySelector(".tempo-slider");
   }
   // Actives pad color change
   activePad() {
@@ -113,6 +114,19 @@ class DrumKit {
       }
     }
   }
+  changeTempo(e) {
+    const tempoText = document.querySelector(".tempo-num");
+    tempoText.innerText = e.target.value;
+  }
+  updateTempo(e) {
+    this.bpm = e.target.value;
+    clearInterval(this.isPlaying);
+    this.isPlaying = null;
+    const playBtn = document.querySelector(".play");
+    if (playBtn.classList.contains("active")) {
+      this.start();
+    }
+  }
 }
 
 const drumKit = new DrumKit();
@@ -143,4 +157,12 @@ drumKit.muteBtns.forEach((btn) => {
   btn.addEventListener("click", function (e) {
     drumKit.mute(e);
   });
+});
+
+drumKit.tempoSlider.addEventListener("input", function (e) {
+  drumKit.changeTempo(e);
+});
+
+drumKit.tempoSlider.addEventListener("change", function (e) {
+  drumKit.updateTempo(e);
 });
